a6fe4420ee21d06d8998f9b55ca56cdcf38a51de
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-06-07 14:31:40 +0100

    finished with the simple age calculator

--- app/src/main/java/com/example/agetominutescalculator/MainActivity.kt
+++ app/src/main/java/com/example/agetominutescalculator/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import android.view.View
+import android.widget.TextView
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +15,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.btnDatePicker)
-        button.setOnClickListener { view ->
-            clickDatePicker(view)
-            // on btn click it calls on the Toast to make sure it works
-            Toast.makeText(this, "button works", Toast.LENGTH_LONG).show()
+        button.setOnClickListener {
+            clickDatePicker()
+
+
         }
     }
 
-    private fun clickDatePicker(view: View){
+    private fun clickDatePicker(){
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { _, _, _, _ -> }, year, month, day).show()
+        DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+            // this sets the dateText from the view on main
+            val date = findViewById<TextView>(R.id.dateText)
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+            date.text = selectedDate
+
+            // this sets the minText from the view on main
+            // Converts date inputs and parses it into date
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate: Date? = sdf.parse(selectedDate)
+            val today = Date()
+            val calc = ((today.time - theDate!!.time)/86400000)*1440
+            val dob = findViewById<TextView>(R.id.minText)
+            dob.text= calc.toString()
+
+            // on (okay from DP) btn click it calls on the Toast to make sure it works
+            Toast.makeText(this, "date was selected", Toast.LENGTH_LONG).show()
+        }, year, month, day).show()
     }
 }
